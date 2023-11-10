876c327aeede9e012204f9666fb633c2dee61833
HullaBrian <beierlejc@gmail.com>
2023-11-10 12:53:16 -0600

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # EventHorizon
+"You can't escape the Event Horizon"
+
+EventHorizon will eventually become a Security Information and Event Management (SIEM) system
--- server/src/cmd.c
+++ server/src/cmd.c
@@ -5,5 +5,4 @@ void help() {
 	printf("Usage: ehconsole [OPTIONS]...\n\n");
 	printf("Options:\n");
 	printf("-h   --help            Prints this help menu\n");
-	printf("-l   --listen  <port>  Listen on a specific port\n");
 }
