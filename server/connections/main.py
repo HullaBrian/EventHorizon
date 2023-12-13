2fa95615678554bcb647a3ba72047cdc6acc246f
hullabrian <beierlejc@gmail.com>
2023-12-12 23:25:29 -0600

    Converted server infrastructure to take UDP traffic

--- server/connections/main.py
+++ server/connections/main.py
@@ -3,44 +3,19 @@ import socketserver
 import threading
 
 
-connections = []
-
-
 class Listener(socketserver.BaseRequestHandler):
     def handle(self):
-        global connections
-        client_address = self.client_address
-        print(f"[+] Server received a connection from {client_address}")
-
-        try:
-            with threading.Lock():
-                connections.append(self.request)
-                print("Connections: ")
-                for connection in connections:
-                    print("\t" + str(connection.getpeername()))
-        except OSError:
-            pass
-
-        welcome_message = "Welcome to the server!"
-        self.request.sendall(welcome_message.encode())
-
-        try:
-            while True:
-                data = self.request.recv(1024)
-                if not data:
-                    break  # Break the loop if no more data is received
-                print(f"Data received from {client_address}:{data.decode()}")
-        except ConnectionResetError:
-            print(f"[-] Connection from {client_address} was reset.")
-        except Exception as e:
-            print(f"[-] Encountered an error while handling connection from {client_address}: {str(e)}")
-        finally:
-            self.request.close()
+        data = self.request[0].strip()
+        print(data)
+        socket = self.request[1]
+        current_thread = threading.current_thread()
+        print(f"[{self.client_address[0]}:{self.client_address[0]}]: {data.decode('utf-8')}")
+        socket.sendto(data.upper(), self.client_address)
 
 
 def listener(host: str, port: int):
     try:
-        with socketserver.ThreadingTCPServer((host, port), Listener) as server:
+        with socketserver.ThreadingUDPServer((host, port), Listener) as server:
             print(f"[+] Server is listening on {host}:{port}")
             server.serve_forever()
             server_thread.join()
